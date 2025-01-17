c92ef38c3180f5a049bdcd30af14ead475f9f85c
csffdmechanism <166661401+csffdmechanism@users.noreply.github.com>
2025-01-17 16:29:55 +0600

    Adding images to the resource card

--- src/components/Blocks/Resources/ResourceCard.js
+++ src/components/Blocks/Resources/ResourceCard.js
@@ -3,34 +3,44 @@ import { formatDate, isArray } from '../../../utils';
 import TagList from '../../Global/Tag/TagList';
 import Link from '../../Global/Link/Link';
 import { ReactSVG } from 'react-svg';
+import { GatsbyImage } from 'gatsby-plugin-image'; // Import GatsbyImage
 import arrowIcon from '../../Icons/resource-arrow.svg';
 
 import './styles.scss';
 
 const ResourceCard = ({ resource, className = '' }) => {
-  const { title, date, introduction, externalUrl, tags = [] } = resource;
+  const { title, date, introduction, externalUrl, tags = [], mainImage } = resource;
 
-  //const intro = introduction.length > 0 ? introduction : title;
+  // Shorten introduction text if it's too long
   const intro = introduction.length > 0 ? introduction.substring(0, 200) + '...' : title;
 
+  // Determine link: either an external URL or resource link
   let link = '';
-  externalUrl != null ?
-    //console.log('externalUrl', externalUrl);
-    link = externalUrl.length > 0 ? externalUrl : resource
-  :
-    link = resource;
+  externalUrl != null ? (link = externalUrl.length > 0 ? externalUrl : resource) : (link = resource);
 
   return (
     <article className={`resource-card ${className}`}>
       <Link to={link}>
+        {/* Check if mainImage exists, and render using GatsbyImage */}
+        {mainImage && mainImage.gatsbyImageData ? (
+          <GatsbyImage
+            image={mainImage.gatsbyImageData}
+            alt={mainImage.alt || 'Resource Image'} // Use alt text if available
+            className="resource-card-image" // Add class for styling
+          />
+        ) : (
+          <div className="resource-card-image-placeholder">No Image Available</div>
+        )}
+
         {isArray(tags) ? <TagList tags={tags} /> : <div className="tags-list" />}
         {date && <span className="date">{formatDate(date)}</span>}
+
         <div className="basic-information">
           <h4>{title}</h4>
         </div>
-        {intro && (
-          <div className="resource-introduction tk-neue" dangerouslySetInnerHTML={{ __html: intro }} />
-        )}
+
+        {intro && <div className="resource-introduction tk-neue" dangerouslySetInnerHTML={{ __html: intro }} />}
+
         <ReactSVG src={arrowIcon} className="btn-img" />
         <div className="custom-btn custom-btn-primary">Read and download</div>
       </Link>
