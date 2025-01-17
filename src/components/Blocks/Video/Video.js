eec121ad850a9f8c83bde8d81dea266ceba91203
Akash <akash@Akashs-MacBook-Pro.local>
2025-01-17 18:45:33 +0600

    Adding code so that it allows external video

--- src/components/Blocks/Video/Video.js
+++ src/components/Blocks/Video/Video.js
@@ -2,23 +2,42 @@ import React from 'react';
 import VideoPlayer from '../../Global/VideoPlayer/VideoPlayer';
 
 function Video({ content, withContainer = false }) {
+  const isExternalVideoUrl = (url) => {
+    // Basic validation for external video URLs
+    return /^(https?:\/\/)(www\.)?(youtube\.com|youtu\.be|vimeo\.com|player\.vimeo\.com|.*\.(mp4|webm|ogg))/.test(url);
+  };
   return (
     <>
-      { content?.video && (
+      {content?.video && (
         <div className={`${withContainer ? 'container' : ''}`}>
           <VideoPlayer video={content?.video} />
         </div>
       )}
-      { !content?.video && (
-      <iframe
-        width="560"
-        height="315"
-        src={content.localVideoUrl}
-        frameBorder="0"
-        allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-        allowFullScreen
-        style={{ maxWidth: '100%' }}
-      ></iframe>
+      {!content?.video && content.localVideoUrl && (
+        <iframe
+          width="560"
+          height="315"
+          src={content.localVideoUrl}
+          frameBorder="0"
+          allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+          style={{ maxWidth: '100%' }}
+        ></iframe>
+      )}
+      {!content?.video && isExternalVideoUrl(content.youtubeUrl) && (
+        <iframe
+          width="560"
+          height="315"
+          src={content.youtubeUrl}
+          frameBorder="0"
+          allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+          style={{ maxWidth: '100%' }}
+        ></iframe>
+      )}
+
+      {!content?.video && !content.localVideoUrl && !isExternalVideoUrl(content.youtubeUrl) && (
+        <p>No valid video source found.</p>
       )}
     </>
   );
